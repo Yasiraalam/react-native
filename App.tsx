a253c056a422bf2d1dde2f79fb54bd9ebbe4d210
Yasir alam <yasiralam981@gmail.com>
2025-07-23 16:05:44 +0530

    [✨] added flatcads

--- App.tsx
+++ App.tsx
@@ -1,28 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import FlatCards from './components/FlatCards';
+import { SafeAreaView } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <SafeAreaView>
+      <View
+        style={[{ backgroundColor: isDarkMode ? '#000' : '#fff' }]}
+      >
+        <FlatCards />
+      </View>
+    </SafeAreaView>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
